82879540efdef3453df1611e76b81d9625b85eeb
Jeremiah Kahanec <jeremiahkahanec@gmail.com>
2015-11-22 18:16:34 -0500

    added fs.h file

---
+++ fs.h
@@ -0,0 +1,55 @@
+#ifndef _FS_H_
+#define _FS_H_
+
+
+#define N_BLOCKS 12
+struct fs_inode {
+	uint16_t  mode; //File Mode
+	uint16_t  uid;  //Owner user id
+	uint32_t  size; // size in bytes
+	uint32_t  atime; //last access time
+	uint32_t  ctime; //creation time
+	uint32_t  mtime; //modification time
+	uint32_t  dtime; //deletion time
+	uint16_t  gid;  //low 16 bits of group id (*)
+	uint16_t  links_count; //links count
+	uint32_t  blocks; //blocks count
+	uint32_t  flags; //file flags
+	
+	uint32_t  i_block[N_BLOCKS]; //pointers to blocks 
+	uint32_t generation; //file version
+	uint32_t file_acl; //File Access Control List
+	uint32_t dir_acl; //Directory Access Control List
+	uint32_t faddr;  //fragment address
+
+	uint8_t frag; //fragment number
+	uint8_t fsize //Fragment Size
+	
+	// The number of the block group which contains this file's inode
+	uint32_t block_group; 
+	
+	struct mutex lock;
+	
+	};
+
+struct fs_super_block {
+	uint32_t s_inodes_count; //Inodes count
+	uint32_t s_blocks_count; //blocks count
+	uint32_t s_r_blocks_count; //reserved blocks count
+	uint32_t s_free_blocks_count; //free blocks count
+	uint32_t s_free_inodes_count; //free inodes count
+	uint32_t s_first_data_block; //first data block
+	uint32_t s_log_block_size; //Block Size
+	uint32_t s_log_frag_size; //fragment size
+	uint32_t s_blocks_per_group; //# Blocks per group
+	uint32_t s_frags_per_group; //# fragments per group
+	uint32_t s_inodes_per_group; //# inodes per group
+	uint32_t s_mtime; //mount time
+	uint32_t s_wtime; //write time
+	uint16_t s_mnt_count; //Mount count
+	uint16_t s_state; //File system state
+	uint16_t s_def_resuid; //Default uid for reserved blocks
+	uint16_t s_def_resgid //Default gid for reserved blocks
+};
+
+#endif
